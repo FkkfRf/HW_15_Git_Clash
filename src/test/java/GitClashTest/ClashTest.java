6a3697698de26edcfe8b7bf0d2e521eeb0c1bd53
FkkfRf <Fkkf.Rf@gmail.com>
2022-09-16 23:24:17 +0300

    первый комит из консоли

---
+++ src/test/java/GitClashTest/ClashTest.java
@@ -0,0 +1,12 @@
+package GitClashTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClashTest {
+    @Test
+    void clashTest(){
+        Assertions.assertTrue(1<2);
+
+    }
+}
