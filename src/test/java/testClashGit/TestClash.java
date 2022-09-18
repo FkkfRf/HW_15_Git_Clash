19c8c37223e7c68f852e6cfe3dc77f7ad2ee34cd
Alla <113384442+FkkfRf@users.noreply.github.com>
2022-09-18 03:03:39 +0300

    Update TestClash.java
    Проверка по False

--- src/test/java/testClashGit/TestClash.java
+++ src/test/java/testClashGit/TestClash.java
@@ -16,11 +16,11 @@ public class TestClash {
     @Test
     void simpleTest1() {
         System.out.println("### simpleTest1 |");
-        Assertions.assertTrue(nxt >= 5 );
+        Assertions.assertFalse(nxt >= 5 );
     }
         @Test
         void simpleTest2(){
             System.out.println("### simpleTest2 |");
-            Assertions.assertTrue(nxt <= 3);
+            Assertions.assertFalse(nxt <= 3);
     }
 }
